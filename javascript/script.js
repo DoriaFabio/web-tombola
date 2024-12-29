e78dcfa94d42b67510668d22060bf168bc21c882
DoriaFabio <doriafabio2001@gmail.com>
2024-12-29 19:27:03 +0000

    do delete number to array

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,9 @@
 console.clear();
 let numeroTombola = [];
+const extraction = document.getElementById("extraction");
+const end = document.getElementById("end");
+const randomNumber = document.getElementById("random");
+
 
 for (let i = 1; i <= 90; i++) {
     numeroTombola.push(i);
@@ -7,9 +11,28 @@ for (let i = 1; i <= 90; i++) {
 console.log(numeroTombola);
 const template = numeroTombola.map((numero) => {
     return `
-    <div class="mycol">
+    <div id=${numero} class="mycol">
         <a class="numeri text-decoration-none text-black">${numero}</a>
     </div>
     `;
 }).join("");
 numero.innerHTML += template;
+let nuovoArray = numeroTombola
+extraction.addEventListener("click", function () {
+    let indice = Math.floor(Math.random() * nuovoArray.length);
+    let random = nuovoArray[indice];
+    randomNumber.innerHTML = random;
+    nuovoArray = nuovoArray.filter(elemento => elemento !== random);
+    console.log(nuovoArray);
+    // numeroTombola.forEach((el) => {
+    //     console.log(el);
+
+    //     if (el == random) {
+    //          el.classList.add("active");
+    //     }
+    // });
+});
+
+end.addEventListener("click", function () {
+    alert("Finito il gioco");
+});
